bae20c135c6d7634756ddbad3a8729f8dd67ebf6
Evgeny Mosunov <eugene.mosunov@saritasa.com>
2018-08-01 14:53:02 +0500

    Change type of implementation of IRepositoryFactory in DI container to singleton.
    Improve package provider documentation.

--- src/LaravelRepositoriesServiceProvider.php
+++ src/LaravelRepositoriesServiceProvider.php
@@ -6,10 +6,19 @@ use Illuminate\Support\ServiceProvider;
 use Saritasa\LaravelRepositories\Repositories\RepositoryFactory;
 use Saritasa\LaravelRepositories\Contracts\IRepositoryFactory;
 
+/**
+ * Package providers. Registers package implementation in DI container.
+ * Make settings needed to correct work.
+ */
 class LaravelRepositoriesServiceProvider extends ServiceProvider
 {
+    /**
+     * Register package implementation in DI container.
+     *
+     * @return void
+     */
     public function register(): void
     {
-        $this->app->bind(IRepositoryFactory::class, RepositoryFactory::class);
+        $this->app->singleton(IRepositoryFactory::class, RepositoryFactory::class);
     }
 }
