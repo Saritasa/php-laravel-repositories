147e70773e024261e71abf39df563f522cec9405
Evgeny Mosunov <eugene.mosunov@saritasa.com>
2018-08-01 15:03:00 +0500

    Add package configuration file.
    Add ability to register custom repositories implementations using
    configuration file.

--- src/LaravelRepositoriesServiceProvider.php
+++ src/LaravelRepositoriesServiceProvider.php
@@ -21,4 +21,37 @@ class LaravelRepositoriesServiceProvider extends ServiceProvider
     {
         $this->app->singleton(IRepositoryFactory::class, RepositoryFactory::class);
     }
+
+    /**
+     * Make package settings needed to correct work.
+     *
+     * @return void
+     */
+    public function boot(): void
+    {
+        $this->publishes(
+            [
+                __DIR__ . '/../config/laravel_repositories.php' =>
+                    $this->app->make('path.config') . DIRECTORY_SEPARATOR . 'laravel_repositories.php',
+            ],
+            'laravel_repositories'
+        );
+        $this->mergeConfigFrom(__DIR__ . '/../config/laravel_repositories.php', 'laravel_repositories');
+
+        $this->registerCustomBindings();
+    }
+
+    /**
+     * Register custom repositories implementations.
+     *
+     * @return void
+     */
+    protected function registerCustomBindings(): void
+    {
+        $repositoryFactory = $this->app->make(IRepositoryFactory::class);
+
+        foreach (config('laravel_repositories.bindings') as $className => $repository) {
+            $repositoryFactory->register($className,$repository);
+        }
+    }
 }
